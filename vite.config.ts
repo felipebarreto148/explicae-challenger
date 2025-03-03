b202ef4c4a0210c883c3c5424834c314fc3733b5
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-03 00:25:41 -0300

    feat: add node types and configure custom paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 // Core
 import { defineConfig } from 'vite'
 
+// Utils
+import path from 'path'
+
 // Plugins
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
