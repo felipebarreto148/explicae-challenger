f232313a28c55e06e53e4e15d8fcee32a8ff447b
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-03 00:26:46 -0300

    feat: create base header and add in app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+// Core
+import { defineAsyncComponent } from "vue";
+
+// Components
+const BaseHeader = defineAsyncComponent(() => import("@/components/Base/Header/index.vue"));
 </script>
 
 <template>
+  <main class="flex flex-col gap-5">
+    <BaseHeader />
+  </main>
 </template>
