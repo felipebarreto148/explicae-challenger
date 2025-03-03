d10f23622c2ebb50ad46f27a6ff5c9ac374cfb79
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-02 23:40:38 -0300

    clear project, install tailwind css and reset css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+// Core
 import { defineConfig } from 'vite'
+
+// Plugins
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 })
